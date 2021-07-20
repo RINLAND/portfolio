30bae9107ca4379c90b0c2347d316f0f1a77c718
RINLAND <summerland829@gmail.com>
2021-07-20 16:51:55 +0900

    JS: Navbar toggle + project UI tweak

--- main.js
+++ main.js
@@ -14,6 +14,13 @@ document.addEventListener('scroll' , ()=> {
     }
 });
 
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=>{
+    navbarMenu.classList.toggle('open');
+
+});
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) =>{
@@ -26,6 +33,7 @@ navbarMenu.addEventListener('click', (event) =>{
         // console.log(event.target.dataset.link);
         // const scrollTo = document.querySelector(link);
         // scrollTo.scrollIntoView({ behavior: 'smooth'});
+        navbarMenu.classList.remove('open');
         scrollIntoView(link);
 
     
